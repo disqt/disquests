305ccde745f894be7f87d32e29f4abe5f34cac0f
Leo <leo.lebleis@gmail.com>
2026-03-20 22:58:32 +0000

    feat: migrate ConfirmScreen to owo-ui
    Rewrite ConfirmScreen extending DisquestsBaseScreen with XML layout.
    Centered dark panel with message label and Yes/No buttons.

--- client/src/main/java/com/disqt/disquests/client/gui/screen/ConfirmScreen.java
+++ client/src/main/java/com/disqt/disquests/client/gui/screen/ConfirmScreen.java
@@ -1,57 +1,40 @@
 package com.disqt.disquests.client.gui.screen;
 
-import com.disqt.disquests.client.gui.helper.Colors;
-import com.disqt.disquests.client.gui.helper.UIHelper;
-import com.disqt.disquests.client.gui.widget.DarkButtonWidget;
-import net.minecraft.client.gui.DrawContext;
+import io.wispforest.owo.ui.component.ButtonComponent;
+import io.wispforest.owo.ui.component.LabelComponent;
+import io.wispforest.owo.ui.container.FlowLayout;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
-public class ConfirmScreen extends BaseScreen {
+public class ConfirmScreen extends DisquestsBaseScreen {
 
     private final Text message;
     private final Runnable onConfirm;
     private final Runnable onCancel;
 
-    public ConfirmScreen(Screen parent, Text message, Runnable onConfirm, Runnable onCancel) {
-        super(Text.of("Confirm"), parent);
+    public ConfirmScreen(@Nullable Screen parent, Text message, Runnable onConfirm, Runnable onCancel) {
+        super(DataSource.asset(Identifier.of("disquests", "confirm_screen")), parent);
         this.message = message;
         this.onConfirm = onConfirm;
         this.onCancel = onCancel;
     }
 
     @Override
-    protected void init() {
-        super.init();
+    protected void build(FlowLayout root) {
+        root.childById(LabelComponent.class, "message-label")
+                .text(message);
 
-        int panelH = 100;
-        int panelY = (this.height - panelH) / 2;
+        root.childById(ButtonComponent.class, "btn-yes")
+                .onPress(b -> onConfirm.run());
 
-        int btnY = panelY + panelH - UIHelper.BUTTON_HEIGHT - UIHelper.OUTER_PADDING;
-
-        UIHelper.createButtonRow(this, btnY, 2, x -> {
-            boolean isLeft = x == UIHelper.getCenteredButtonStartX(this.width, 2);
-            this.addDrawableChild(new DarkButtonWidget(
-                    x, btnY, UIHelper.BUTTON_WIDTH, UIHelper.BUTTON_HEIGHT,
-                    Text.of(isLeft ? "Yes" : "No"),
-                    b -> { if (isLeft) onConfirm.run(); else onCancel.run(); }
-            ));
-        });
+        root.childById(ButtonComponent.class, "btn-no")
+                .onPress(b -> onCancel.run());
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        int panelW = Math.min(this.width - 80, 360);
-        int panelH = 100;
-        int panelX = (this.width - panelW) / 2;
-        int panelY = (this.height - panelH) / 2;
-
-        panelH = panelH - (UIHelper.BUTTON_HEIGHT + (UIHelper.OUTER_PADDING * 2));
-        UIHelper.drawPanel(context, panelX, panelY, panelW, panelH);
-        super.render(context, mouseX, mouseY, delta);
-
-        int textMaxWidth = panelW - 24; // 12px padding on each side
-        String trimmedMessage = this.textRenderer.trimToWidth(this.message.getString(), textMaxWidth);
-        context.drawTextWithShadow(this.textRenderer, Text.literal(trimmedMessage), panelX + 12, panelY + 12, Colors.TEXT_PRIMARY);
+    public boolean shouldPause() {
+        return false;
     }
 }
